a302c2195b5cf2894cbdfc1367b4a6dc3c437595
toyPoodle <toyPoodle@users.noreply.github.com>
2015-05-14 09:39:01 +0200

    - added verify() for clearer error messages

--- src/test/java/de/vik/testrail2java/testhelpers/Mockups.java
+++ src/test/java/de/vik/testrail2java/testhelpers/Mockups.java
@@ -1,38 +1,48 @@
 package de.vik.testrail2java.testhelpers;
 
-import de.vik.testrail2java.net.APIClient;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+import de.vik.testrail2java.net.APIClient;
+
 import static de.vik.testrail2java.testhelpers.MoreMatchers.uri;
-import static org.hamcrest.Matchers.sameInstance;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.AdditionalMatchers.aryEq;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class Mockups {
 
     public static <R> void testGetList(Class<R> resultType, final String uriString, Function<APIClient, List<R>> targetCall) {
         List<R> expected = new ArrayList<>();
+        expected.add(mock(resultType, "pseudo result list item"));
         APIClient client = mock(APIClient.class);
-
         when(client.getList(eq(resultType), uri(uriString))).thenReturn(expected);
 
-        assertThat(targetCall.apply(client), sameInstance(expected));
+        final List<R> actual = targetCall.apply(client);
+
+        //Technically not needed, but helps when there is a parameter mismatch
+        verify(client).getList(eq(resultType), uri(uriString));
+        assertThat(actual, equalTo(expected));
     }
 
     public static <R> void testGetItem(Class<R> resultType, final String uriString, Function<APIClient, R> targetCall) {
         R expected = mock(resultType);
         APIClient client = mock(APIClient.class);
+        when(client.get(eq(resultType), uri(uriString))).thenReturn(expected);
 
-        when(client.get(eq(resultType), MoreMatchers.uri(uriString))).thenReturn(expected);
+        final R actual = targetCall.apply(client);
 
-        assertThat(targetCall.apply(client), sameInstance(expected));
+        //Technically not needed, but helps when there is a parameter mismatch
+        verify(client).get(eq(resultType), uri(uriString));
+        assertThat(actual, equalTo(expected));
     }
 
     public static <D> void testSubmissionWithData(String uriString, Class<D> dataClass, String[] allowedFields, Consumer<D> testDataSetup, BiFunction<APIClient, D, D> targetCall) {
@@ -44,19 +54,22 @@ public class Mockups {
         when(apiClient.post(uri(uriString), eq(data), aryEq(allowedFields), eq(dataClass))).thenReturn(expected);
 
         final D actual = targetCall.apply(apiClient, data);
+
+        //Request should be submitted only once
         verify(apiClient, times(1)).post(uri(uriString), eq(data), aryEq(allowedFields), eq(dataClass));
-        assertThat(actual, sameInstance(expected));
+        assertThat(actual, equalTo(expected));
     }
 
     public static <R> void testSubmissionWithoutData(String uriString, Class<R> resultClass, Function<APIClient, R> targetCall) {
         R expected = mock(resultClass);
         APIClient apiClient = mock(APIClient.class);
-
         when(apiClient.post(uri(uriString), eq(resultClass))).thenReturn(expected);
 
         final R actual = targetCall.apply(apiClient);
+
+        //Request should be submitted only once
         verify(apiClient, times(1)).post(uri(uriString), eq(resultClass));
-        assertThat(actual, sameInstance(expected));
+        assertThat(actual, equalTo(expected));
     }
 
     public static void testSubmissionWithoutResultAndData(String uriString, Consumer<APIClient> targetCall) {
@@ -64,6 +77,7 @@ public class Mockups {
 
         targetCall.accept(apiClient);
 
+        //Request should be submitted only once
         verify(apiClient, times(1)).post(uri(uriString));
     }
 }
