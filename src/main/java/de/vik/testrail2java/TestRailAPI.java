734d7df5b27bbc0e6d53b89df07177817ba57ea8
toyPoodle <toyPoodle@users.noreply.github.com>
2015-08-22 18:53:17 +0200

    - added creator methods to TestRailAPI class

--- src/main/java/de/vik/testrail2java/TestRailAPI.java
+++ src/main/java/de/vik/testrail2java/TestRailAPI.java
@@ -1,9 +1,24 @@
 package de.vik.testrail2java;
 
+import de.vik.testrail2java.controller.CaseFields;
+import de.vik.testrail2java.controller.Cases;
+import de.vik.testrail2java.controller.Configurations;
+import de.vik.testrail2java.controller.Milestones;
+import de.vik.testrail2java.controller.Plans;
+import de.vik.testrail2java.controller.Priorities;
+import de.vik.testrail2java.controller.Projects;
+import de.vik.testrail2java.controller.ResultFields;
+import de.vik.testrail2java.controller.Results;
+import de.vik.testrail2java.controller.Runs;
+import de.vik.testrail2java.controller.Sections;
+import de.vik.testrail2java.controller.Statuses;
+import de.vik.testrail2java.controller.Suites;
+import de.vik.testrail2java.controller.Tests;
+import de.vik.testrail2java.controller.Types;
+import de.vik.testrail2java.controller.Users;
 import de.vik.testrail2java.net.APIClient;
 import de.vik.testrail2java.net.RestClient;
 import de.vik.testrail2java.serialization.GsonBuilder;
-import de.vik.testrail2java.controller.Cases;
 
 
 public class TestRailAPI {
@@ -24,8 +39,67 @@ public class TestRailAPI {
         return new APIClient(client, new GsonBuilder());
     }
 
+    public CaseFields caseFields() {
+        return new CaseFields(getApiClient());
+    }
+
     public Cases cases() {
         return new Cases(getApiClient());
     }
 
+    public Configurations configurations() {
+        return new Configurations(getApiClient());
+    }
+
+    public Milestones milestones() {
+        return new Milestones(getApiClient());
+    }
+
+    public Plans plans() {
+        return new Plans(getApiClient());
+    }
+
+    public Priorities priorities() {
+        return new Priorities(getApiClient());
+    }
+
+    public Projects projects() {
+        return new Projects(getApiClient());
+    }
+
+    public ResultFields resultFields() {
+        return new ResultFields(getApiClient());
+    }
+
+    public Results results() {
+        return new Results(getApiClient());
+    }
+
+    public Runs runs() {
+        return new Runs(getApiClient());
+    }
+
+    public Sections sections() {
+        return new Sections(getApiClient());
+    }
+
+    public Statuses statuses() {
+        return new Statuses(getApiClient());
+    }
+
+    public Suites suites() {
+        return new Suites(getApiClient());
+    }
+
+    public Tests tests() {
+        return new Tests(getApiClient());
+    }
+
+    public Types types() {
+        return new Types(getApiClient());
+    }
+
+    public Users users() {
+        return new Users(getApiClient());
+    }
 }
